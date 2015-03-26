ead066638a44b197ff407fc01dabb95fb705a015
Stephen Palmer <stephen@fun-machine.com>
2015-03-26 15:26:26 -0500

    Added views, images, fonts, etc. for board display

--- api/controllers/BoardController.js
+++ api/controllers/BoardController.js
@@ -5,35 +5,16 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-var _ = require('lodash');
-
 module.exports = {
   findOne: function(req, res) {
-    Board.findOne(req.options.id)
-      .populate("taps")
+    Board.findOneAndPopulate(req.options.id)
       .then(function(board) {
-        var tapKegs = Keg.find({
-              id: _.pluck(_.sortBy(board.taps, "sortIndex"), "keg")
-          })
-          .populate("contents")
-          .then(function(results) {
-            return _.indexBy(results, 'id');
-          });
-
-        return [board, tapKegs];
-      })
-      .spread(function(board, tapKegs) {
-        board.taps = _.map(board.taps, function(tap) {
-          tap.keg = tapKegs[tap.keg];
-          return tap;
+        res.view('board/show', {
+          board: board
         });
-
-        res.ok(board);
       })
       .catch(function(err) {
-        if(err) {
-         return res.serverError(err);
-        }
+        res.serverError(err);
       });
   }
 };
--- api/models/Board.js
+++ api/models/Board.js
@@ -4,6 +4,7 @@
 * @description :: A collection of taps to display
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var Promise = require('bluebird');
 
 module.exports = {
 
@@ -15,6 +16,35 @@ module.exports = {
       collection: 'tap',
       via: 'boards'
     }
+  },
+
+  findOneAndPopulate: function(id) {
+    return new Promise(function(resolve, reject) {
+      Board.findOne(id)
+        .populate("taps")
+        .then(function(board) {
+          var tapKegs = Keg.find({
+            id: _.pluck(_.sortBy(board.taps, "sortIndex"), "keg")
+          })
+            .populate("contents")
+            .then(function(results) {
+              return _.indexBy(results, 'id');
+            });
+
+          return [board, tapKegs];
+        })
+        .spread(function(board, tapKegs) {
+          board.taps = _.map(board.taps, function(tap) {
+            tap.keg = tapKegs[tap.keg];
+            return tap;
+          });
+
+          resolve(board);
+        })
+        .catch(function(err) {
+          reject(err);
+        });
+    });
   }
 };
 
